5844506ff40ab4d46185704f2bc477c2b2e57d27
yorranan <yorranan.costa@outlook.com>
2023-10-19 13:13:29 -0300

    2a aula

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  visor: number = 0;
+  visor: string = '0';
+  valor1!: number;
+  valor2!: number;
+  operacao!: number;
   constructor() {}
+
+  addNumber(valor: string)  {
+    if ((this.visor.length == 1) && (this.visor == '0')) {
+      this.visor = valor;
+    }
+    else {
+      this.visor += valor;
+    }
+  }
+
+  addOperation(valor: number) {
+    this.operacao = valor;
+    this.valor1 = +this.visor;
+  }
+
+  calcular() {
+    this.valor2 = +this.visor;
+    switch(this.operacao) {
+      case 0: {
+        this.zerar()
+        this.visor = "" + (this.valor1 + this.valor2);
+        break;
+      }
+      case 1: {
+        this.zerar()
+        this.visor = "" + (this.valor1 - this.valor2)
+        break;
+      }
+      case 2: {
+        this.zerar()
+        this.visor = "" + (this.valor1 * this.valor2)
+        break;
+      }
+      case 3: {
+        this.zerar()
+        this.visor = "" + (this.valor1 / this.valor2)
+        break;
+      }
+    }
+  }
+
+  zerar() {
+    this.visor = '0';
+  }
 }
 
